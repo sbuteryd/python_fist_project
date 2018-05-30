6b581a288ab3727e3cfa322ba891c931707dcbfc
sbuteryd@qq.com <34436143+sbuteryd@users.noreply.github.com>
2018-05-30 17:12:24 +0800

    completion

--- Task0.py
+++ Task0.py
@@ -20,3 +20,16 @@ with open('calls.csv', 'r') as f:
 "Last record of calls, <incoming number> calls <answering number> at time <time>, lasting <during> seconds"
 """
 
+def message_print(number):
+    # list destructuring
+    f_number,f_anwer,f_time= texts[number]
+    message = "First record of texts, {} texts {} at time {}".format(f_number,f_anwer,f_time)
+    return  message
+
+def  phone_number(figure):
+    f_number,f_anwer,f_time,f_second,= calls[figure]
+    message = "Last record of calls, {} calls {} at time {}, lasting {} seconds".format(f_number,f_anwer,f_time,f_second)
+    return message
+
+print(message_print(0))
+print(phone_number(0))
